bb248e39a8dfca9d3e823b841ef8d6c0c3499e7b
Christian Grewell <christian@nyu.edu>
2022-10-21 12:24:31 -0400

    firebase kind of

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAb6fJ-n0sJIk4nldgEaqLfpBDY9XbdrhY",
+  authDomain: "whale-3daba.firebaseapp.com",
+  databaseURL: "https://whale-3daba-default-rtdb.firebaseio.com",
+  projectId: "whale-3daba",
+  storageBucket: "whale-3daba.appspot.com",
+  messagingSenderId: "588785321223",
+  appId: "1:588785321223:web:2468b26e564341349c71a2",
+  measurementId: "G-04DGX728VE"
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+function writeUserData(userId, name) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+    });
+  }
+
+writeUserData('clg236', 'christian')
+
 require('dotenv').config({
     'path' : __dirname + '/.env'
 })
